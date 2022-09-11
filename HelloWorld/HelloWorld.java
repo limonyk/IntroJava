4526fd726e00ff46c2063695464fcb9756b83339
limonyk <wangsalem.m@gmail.com>
2022-09-10 20:01:13 -0400

    CIS1500

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("1 - My partner's name is Xiaoling Li and goes by Alicia Li. Her major is accounting, and she is taking the class to expand her career path.\n" +
+                "2 - Her previous programming experience is SQL and Macro.\n" +
+                "3 - Likes to cook and write.\n" +
+                "4 - Plans to go to the Lego Land Resort in NY with her family in September.\n" +
+                "5 - She's excited to gain more hands-on experience with programming.");
+    }
+}
